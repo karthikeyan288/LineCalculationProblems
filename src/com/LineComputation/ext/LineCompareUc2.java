b0bbd91c013cdfa8be644e224539b9571eb96a0d
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-10-31 16:01:40 +0530

    ADDING : Find both line are equal or not

---
+++ src/com/LineComputation/ext/LineCompareUc2.java
@@ -0,0 +1,33 @@
+package com.LineComputation.ext;
+
+import java.util.Scanner;
+
+//check two line is equal
+public class LineCompareUc2 {
+	public static void main(String[] args) {
+		//the line 1 cordinates
+		Scanner endPoint=new Scanner(System.in);
+		System.out.println("Enter the endpoint of line1 : ");
+		int x1 = endPoint.nextInt();
+		int x2 = endPoint.nextInt();
+		int y1 = endPoint.nextInt();
+		int y2 = endPoint.nextInt();
+		double total1 =((x2-x1)*2+(y2-y1)*2);
+		double Equation1=Math.floor(Math.sqrt(total1));   //.sqrt((x2-x1)*2);    //+(y2-y1)*2);
+		//the line 2 cordinates
+		Scanner endPoint1=new Scanner(System.in);
+		System.out.println("Enter the endpoint of line2 : ");
+		int p1 = endPoint1.nextInt();
+		int p2 = endPoint1.nextInt();
+		int q1 = endPoint1.nextInt();
+		int q2 = endPoint1.nextInt();
+		double total2 =((p2-p1)*2+(q2-q1)*2);
+		double Equation2 =Math.floor(Math.sqrt(total2));
+		
+		if(Equation1==Equation2)
+			System.out.println("The both line are equal");
+		else
+			System.out.println("The both line are not equal");
+	}
+
+}
